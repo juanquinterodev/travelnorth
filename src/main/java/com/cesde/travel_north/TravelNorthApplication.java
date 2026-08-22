13cb7695f77be2308d7ab93a1ba5a78ed74f524e
juanquinterodev <juanquintero2007@gmail.com>
2026-08-22 14:42:53 -0500

    primer empujon

---
+++ src/main/java/com/cesde/travel_north/TravelNorthApplication.java
@@ -0,0 +1,13 @@
+package com.cesde.travel_north;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelNorthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelNorthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesde/travel_north/TravelNorthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.travel_north;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelNorthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
